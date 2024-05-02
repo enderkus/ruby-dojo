4dd1867f4b1cd63640259f5fb8fc4f00000e5371
Ender KUŞ <enderkus@msn.com>
2024-05-02 11:10:52 +0300

    Update puts-example.rb

--- puts-example.rb
+++ puts-example.rb
@@ -1,2 +1,7 @@
 puts("hello Ruby!")
 puts(4-1)
+
+# Ruby programlama dilinde parantezleri kullanip kullanmamaniz bir sorun olusturmaz ustteki ciktilari asagidaki gibi de alabilirsiniz
+
+puts "hello Ruby!"
+puts 4-1
