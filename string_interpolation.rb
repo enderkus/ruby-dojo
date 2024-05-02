c648e72c446ff37282b8b2aed13fae2f66a4ece6
Ender KUŞ <enderkus@msn.com>
2024-05-02 11:17:14 +0300

    Ruby string_interpolation.rb example

---
+++ string_interpolation.rb
@@ -0,0 +1,10 @@
+name = "Ruby"
+release_date = 1992
+
+puts "Hello #{name}" # => Hello Ruby
+
+puts "The #{name} programming language was released in #{release_date}." # => The Ruby programming language was released in 1995.
+
+# Dikkatli olun " (Cift tirnak) ile ' (Tek tirnak) farkli sonuclar cikarir.
+
+puts 'The #{name} programming language was released in #{release_date}.' # =>  The #{name} programming language was released in #{release_date}.
